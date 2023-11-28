b2ddeead5d487d840f0e4b25570aebd9ec762198
KenanBolat <kenan23@gmail.com>
2023-11-28 11:01:55 +0300

    check ftp

--- jupyter/test_files_and_folders.py
+++ jupyter/test_files_and_folders.py
@@ -4,23 +4,29 @@ import requests
 
 
 class CheckProducts(object):
-    def __init__(self, satellite_mission):
-        self.satellite_mission = satellite_mission
+    def __init__(self):
+        self._satellite_mission = None
         self.host = 'localhost:8000'  # Main server host
         self.ftp = ftplib.FTP()
+        self.config = None  # Configuration of the mission
+        self.available_missions = [row['satellite_mission'] for row in self.get_missions()[0]]  # List of missions
+
+    def satellite_mission(self, satellite_mission):
+        self._satellite_mission = satellite_mission
         self.config = self.get_config()[0]
-        self.missions = [row['satellite_mission'] for row in self.get_missions()[0]] # List of missions
 
     def get_config(self):
+        if self._satellite_mission not in self.available_missions or self._satellite_mission is None:
+            print(f"Invalid mission name: {self._satellite_mission}")
+            assert "Invalid mission name"
 
-        response = requests.get(f"http://{self.host}/api/configuration/{self.satellite_mission}")
+        response = requests.get(f"http://{self.host}/api/configuration/{self._satellite_mission}")
         try:
             if response.status_code == 200:
                 return response.json(), None
         except Exception as e:
             print(e)
             return None, (response.text, response.status_code)
-
 
     def get_missions(self):
         response = requests.get(f"http://{self.host}/api/configuration")
@@ -35,8 +41,7 @@ class CheckProducts(object):
         if self.config:
             self.ftp = ftplib.FTP()
             self.ftp.connect(self.config['ftp_server'], self.config['ftp_port'])
-            self.ftp.login(self.config['ftp_user_name'],self.config['ftp_password'])
-
+            self.ftp.login(self.config['ftp_user_name'], self.config['ftp_password'])
 
     def list_ftp_dir(self, path):
         """ Lists a directory on the FTP server. """
@@ -61,33 +66,28 @@ class CheckProducts(object):
         # Adjust the regular expression pattern to match your timestamp format
         return re.match(r'^\d{12}$', dirname) is not None
 
-    def get_sub_directory(self):
-
-        response = requests.get(f"http://{self.host}/api/configuration/{self.satellite_mission}")
-        try:
-            if response.status_code == 200:
-                return response.json()['folder_locations'], None
-        except:
-            return None, (response.text, response.status_code)
-
     def check_subdirectories(self, timestamp_dir):
         """ Checks for required subdirectories in a timestamped directory. """
-        required_subdirs = self.get_sub_directory()
+        required_subdirs = self.config['folder_locations'].values()
 
-        path = f"/{self.satellite_mission}/{timestamp_dir}"
+        path = f"/{self._satellite_mission}/{timestamp_dir}"
         subdirs = self.parse_directory_listing(self.list_ftp_dir(path))
 
         return all(subdir in subdirs for subdir in required_subdirs)
 
     # Main routine
-    def main(self):
+    def check(self):
+        if self._satellite_mission not in self.available_missions or self._satellite_mission is None:
+            print(f"Invalid mission name: {self._satellite_mission}")
+            assert "Invalid mission name"
 
         try:
             self.connect()
 
             # List the root directory or the specific directory where the timestamped folders are expected
-            root_dir_listing = self.list_ftp_dir(f'/{self.satellite_mission}')
-            timestamped_dirs = [dir for dir in self.parse_directory_listing(root_dir_listing) if self.is_valid_timestamped_dir(dir)]
+            root_dir_listing = self.list_ftp_dir(f'/{self._satellite_mission}')
+            timestamped_dirs = [dir for dir in self.parse_directory_listing(root_dir_listing) if
+                                self.is_valid_timestamped_dir(dir)]
 
             for timestamp_dir in timestamped_dirs:
                 if self.check_subdirectories(timestamp_dir):
@@ -101,9 +101,8 @@ class CheckProducts(object):
 
 
 if __name__ == '__main__':
-
-    a = CheckProducts('MSG')
-    a.main()
-    b = a.get_missions()
-    # CheckProducts('IODC').main()
-    # CheckProducts('IODC2').main()
+    a = CheckProducts()
+    a.get_missions()
+    a.satellite_mission('MSG')
+    a.satellite_mission('IODC')
+    a.check()
