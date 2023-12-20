fef6fd0303bc8f79f2ae345608ae5636ca0a9061
KenanBolat <kenan23@gmail.com>
2023-12-21 01:02:23 +0300

    fix trailing forward slash problem

--- data_retrieval/file_downloader/server.py
+++ data_retrieval/file_downloader/server.py
@@ -2,10 +2,9 @@ import os
 import pika
 import json
 import requests
+
 from dataconverter.communication.message_broker_if import RabbitMQInterface as rabbitmq
 from dataconverter.utils.data_checker import CheckProducts as checker
-import os
-import requests
 
 
 class FileDownloadConsumer:
@@ -25,15 +24,14 @@ class FileDownloadConsumer:
                 "satellite_mission": message_body['mission'],
                 "date_tag": message_body['date'],
             }
-            # headers = {'Authorization': f'Token {os.environ.get("TOKEN")}', 'Content-Type': 'application/json'}
-            headers = {'Authorization': f'Token {os.environ.get("TOKEN", "ef2be80b3918efc09ca24c3cf48daaccf85b9b27")}',
-                       'Content-Type': 'application/json'}
-
-            response = requests.get(f"http://app:8000/api/data", params=payload, headers=headers)
+            headers = {'Authorization': f'Token {os.environ.get("TOKEN")}', 'Content-Type': 'application/json'}
+            response = requests.get(f"http://{os.environ.get('CORE_APP', 'app')}:8000/api/data/", params=payload,
+                                    headers=headers)
             if response.status_code == 200 and response.json()[0]['satellite_mission'] == payload['satellite_mission']:
                 files = response.json()[0]['files']
                 self.download_files(payload['satellite_mission'], files)
             channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+
         except Exception as e:
             print(f"Failed to process message: {e}")
             # Requeue the message for future processing
