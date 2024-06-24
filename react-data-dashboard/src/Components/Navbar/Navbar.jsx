60970a93975e23aa6d5de7d0ef353ff04fb59026
Kenan Bolat <kenan.bolat@yandex.com.tr>
2024-06-24 16:22:02 +0300

    update links

--- react-data-dashboard/src/Components/Navbar/Navbar.jsx
+++ react-data-dashboard/src/Components/Navbar/Navbar.jsx
@@ -12,8 +12,12 @@ const Navbar = () => {
       
         <ul className="nav-menu">
           <li onClick={()=>{setMenu("home")}}><Link to="home">HOME</Link>{menu==="home"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("msg")}}><Link to="msg">MSG</Link>{menu==="msg"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("datatable")}}><Link to="datatable">Datalist</Link>{menu==="datatable"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("channels")}}><Link to="channels">Latest Data</Link>{menu==="channels"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("map")}}><Link to="map">Map</Link>{menu==="map"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("products")}}><Link to="products">Products</Link>{menu==="products"?<hr/>:<></>}</li>
           
+          <li onClick={()=>{setMenu("msg")}}><Link to="msg">MSG</Link>{menu==="msg"?<hr/>:<></>}</li>
           <li onClick={()=>{setMenu("iodc")}}><Link to="iodc">IODC</Link>{menu==="iodc"?<hr/>:<></>}</li>
           <li onClick={()=>{setMenu("rss")}}><Link to="rss">RSS</Link>{menu==="rss"?<hr/>:<></>}</li>
           <li onClick={()=>{setMenu("others")}}><Link to="others">Others</Link>{menu==="others"?<hr/>:<></>}</li>
