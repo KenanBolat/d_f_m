b92c1f313c0b6153c0c7c2d4ce51558aa6fcfafb
Kenan Bolat <kenan.bolat@yandex.com.tr>
2024-07-10 17:26:40 +0300

    add datepicker

--- sil/app.js
+++ sil/app.js
@@ -1,85 +1,158 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const map = L.map('map').setView([37.5, 33.5], 6);
+    const map = L.map('map').setView([37.5, 33.5], 6);
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-  }).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(map);
 
-  const imageMosaicLayerUrl = 'http://localhost:8080/geoserver/tmet/wms';
-  const postgisLayerUrl = 'http://localhost:8080/geoserver/tmet/wms';
-  const params = {
-      layers: 'tmet:aoi',
-      format: 'image/png',
-      transparent: true,
-      time: '2023-08-16T08:45:00.000Z',
-      dim_mission: 'XYZ',
-      dim_channel: 'WV_062'
-  };
+    const imageMosaicLayerUrl = 'http://localhost:8080/geoserver/tmet/wms';
+    const postgisLayerUrl = 'http://localhost:8080/geoserver/tmet/ows';
+    let currentLayers = [];
+    const params = {
+        layers: ['tmet:aoi'],  // Add all the layers you need
+        format: 'image/png',
+        transparent: true,
+        time: '',
+        dim_mission: 'XYZ',
+        dim_channel: 'WV_062'
+    };
 
-  L.tileLayer.wms(imageMosaicLayerUrl, params).addTo(map);
-  L.tileLayer.wms(postgisLayerUrl, {layers:'tmet:border', transparent: true, format: 'image/png',}).addTo(map);
+    const initializeMapLayers = () => {
+        currentLayers.forEach(layer => map.removeLayer(layer));
+        currentLayers = params.layers.map(layer => {
+            return L.tileLayer.wms(imageMosaicLayerUrl, {
+                layers: layer,
+                format: params.format,
+                transparent: params.transparent,
+                time: params.time,
+                DIM_MISSION: params.dim_mission,
+                DIM_CHANNEL: params.dim_channel
+            }).addTo(map);
+        });
+    };
 
-  
-  const infoDiv = document.getElementById('info');
+    L.tileLayer.wms(postgisLayerUrl, {
+        layers: 'tmet:border',
+        transparent: true,
+        format: 'image/png',
+    }).addTo(map);
 
-  map.on('click', function (e) {
-      const latlng = e.latlng;
-      const containerPoint = map.latLngToContainerPoint(latlng);
-      const size = map.getSize();
-      const bbox = map.getBounds().toBBoxString();
-      const crs = map.options.crs.code;
+    const infoDiv = document.getElementById('info');
+    const legendImage = document.getElementById('legend-image');
 
-      let mosaicData = null;
-      let postgisData = null;
 
-      // GetFeatureInfo request for ImageMosaic layer
-      const mosaicUrl = new URL(imageMosaicLayerUrl);
-      mosaicUrl.searchParams.set('SERVICE', 'WMS');
-      mosaicUrl.searchParams.set('VERSION', '1.1.1');
-      mosaicUrl.searchParams.set('REQUEST', 'GetFeatureInfo');
-      mosaicUrl.searchParams.set('LAYERS', 'tmet:aoi');
-      mosaicUrl.searchParams.set('QUERY_LAYERS', 'tmet:aoi');
-      mosaicUrl.searchParams.set('INFO_FORMAT', 'application/json');
-      mosaicUrl.searchParams.set('FEATURE_COUNT', '1');
-      mosaicUrl.searchParams.set('X', Math.floor(containerPoint.x));
-      mosaicUrl.searchParams.set('Y', Math.floor(containerPoint.y));
-      mosaicUrl.searchParams.set('SRS', 'EPSG:4326');
-      mosaicUrl.searchParams.set('WIDTH', size.x);
-      mosaicUrl.searchParams.set('HEIGHT', size.y);
-      mosaicUrl.searchParams.set('BBOX', bbox);
-      mosaicUrl.searchParams.set('TIME', params.time);
-      mosaicUrl.searchParams.set('DIM_MISSION', params.dim_mission);
-      mosaicUrl.searchParams.set('DIM_CHANNEL', params.dim_channel);
+    
+    const updateLegend = () => {
+        const legendUrl = `${imageMosaicLayerUrl}?SERVICE=WMS&VERSION=1.1.0&REQUEST=GetLegendGraphic&LAYER=${params.layers[0]}&format=image/png&STYLE=&DIM_MISSION=${params.dim_mission}&DIM_CHANNEL=${params.dim_channel}`;
+        legendImage.src = legendUrl;
+    };
 
-      axios.get(mosaicUrl.toString()).then(mosaicResponse => {
-         mosaicData = mosaicResponse.data;
+    const updateMap = () => {
+        initializeMapLayers();
+        updateLegend();
+    };
 
-        // WFS GetFeature request for PostGIS layer with CQL_FILTER
-        const postgisUrl = new URL(postgisLayerUrl);
-        postgisUrl.searchParams.set('service', 'WFS');
-        postgisUrl.searchParams.set('version', '1.0.0');
-        postgisUrl.searchParams.set('request', 'GetFeature');
-        postgisUrl.searchParams.set('typeName', 'tmet:aoi_fs');
-        postgisUrl.searchParams.set('outputFormat', 'application/json');
-        //ingestion='2023-08-14 08:45:00.000' 
-        postgisUrl.searchParams.set('CQL_FILTER', 
-                                                    `mission like '${params.dim_mission}' 
-                                                    AND ingestion='${params.time.replace('T', ' ').replace('Z', '')}' 
-                                                    AND channel='${params.dim_channel}' 
-                                                    AND Intersects(the_geom, POINT(${latlng.lng} ${latlng.lat }))`);
 
-        return axios.get(postgisUrl.toString());
-    }).then(postgisResponse => {
-        postgisData = postgisResponse.data;
-        console.log('PostGIS Data:', postgisData);
-        debugger
+    const formatDateToLocalISO = (date) => {
+        const tzoffset = date.getTimezoneOffset() * 60000; //offset in milliseconds
+        const localISOTime = (new Date(date - tzoffset)).toISOString().slice(0, -1);
+        return localISOTime+'Z';
+    };
 
-        // Display results
-        infoDiv.innerHTML = `<h4>Clicked Point: ${latlng.lat}, ${latlng.lng}</h4>`;
-        infoDiv.innerHTML += `<h4>ImageMosaic Data:</h4><pre>${JSON.stringify(mosaicData, null, 2)}</pre>`;
-        infoDiv.innerHTML += `<h4>PostGIS Data:</h4><pre>${JSON.stringify(postgisData, null, 2)}</pre>`;
-    }).catch(error => {
-        console.error('Error fetching data:', error);
+    let startDatePicker = flatpickr("#start-date", {
+        enableTime: true,
+        dateFormat: "Y-m-d H:i:S",
+        defaultDate: "2023-08-14 08:45:00",
+        onChange: function(selectedDates, dateStr, instance) {
+            params.time = selectedDates[0] ? formatDateToLocalISO(selectedDates[0]) : '';
+
+        }
     });
-});
+
+    let endDatePicker = flatpickr("#end-date", {
+        enableTime: true,
+        dateFormat: "Y-m-d H:i:S",
+        defaultDate: "2023-08-14 08:45:00",
+        onChange: function(selectedDates, dateStr, instance) {
+            params.end_time = selectedDates[0] ? formatDateToLocalISO(selectedDates[0]) : '';
+        }
+    });
+
+    document.getElementById('search-button').addEventListener('click', function () {
+        params.dim_mission = document.getElementById('mission-select').value;
+        params.dim_channel = document.getElementById('channel-select').value;
+        debugger
+        if (params.time === '') {
+            const selectedDate = startDatePicker.selectedDates[0];
+            params.time = selectedDate ? formatDateToLocalISO(selectedDate) : formatDateToLocalISO(new Date());
+        }
+        updateMap();
+    });
+
+    map.on('click', function (e) {
+        const latlng = e.latlng;
+        const containerPoint = map.latLngToContainerPoint(latlng);
+        const size = map.getSize();
+        const bbox = map.getBounds().toBBoxString();
+        const crs = map.options.crs.code;
+
+        let mosaicData = [];
+        let postgisData = null;
+
+        const requests = params.layers.map(layer => {
+            // GetFeatureInfo request for each ImageMosaic layer
+            const mosaicUrl = new URL(imageMosaicLayerUrl);
+            mosaicUrl.searchParams.set('SERVICE', 'WMS');
+            mosaicUrl.searchParams.set('VERSION', '1.1.1');
+            mosaicUrl.searchParams.set('REQUEST', 'GetFeatureInfo');
+            mosaicUrl.searchParams.set('LAYERS', layer);
+            mosaicUrl.searchParams.set('QUERY_LAYERS', layer);
+            mosaicUrl.searchParams.set('INFO_FORMAT', 'application/json');
+            mosaicUrl.searchParams.set('FEATURE_COUNT', '1');
+            mosaicUrl.searchParams.set('X', Math.floor(containerPoint.x));
+            mosaicUrl.searchParams.set('Y', Math.floor(containerPoint.y));
+            mosaicUrl.searchParams.set('SRS', 'EPSG:4326');
+            mosaicUrl.searchParams.set('WIDTH', size.x);
+            mosaicUrl.searchParams.set('HEIGHT', size.y);
+            mosaicUrl.searchParams.set('BBOX', bbox);
+            mosaicUrl.searchParams.set('TIME', params.time);
+            mosaicUrl.searchParams.set('DIM_MISSION', params.dim_mission);
+            mosaicUrl.searchParams.set('DIM_CHANNEL', params.dim_channel);
+
+            return axios.get(mosaicUrl.toString());
+        });
+
+        Promise.all(requests).then(responses => {
+            mosaicData = responses.map(response => response.data);
+
+            // WFS GetFeature request for PostGIS layer with CQL_FILTER
+            const postgisUrl = new URL(postgisLayerUrl);
+            postgisUrl.searchParams.set('service', 'WFS');
+            postgisUrl.searchParams.set('version', '1.0.0');
+            postgisUrl.searchParams.set('request', 'GetFeature');
+            postgisUrl.searchParams.set('typeName', 'tmet:aoi_fs');
+            postgisUrl.searchParams.set('outputFormat', 'application/json');
+            postgisUrl.searchParams.set('CQL_FILTER',
+                `mission like '${params.dim_mission}' AND
+                ingestion='${params.time.replace('T', ' ').replace('Z', '')}' AND
+                channel='${params.dim_channel}' AND
+                Intersects(the_geom, POINT(${latlng.lng} ${latlng.lat}))`);
+
+            return axios.get(postgisUrl.toString());
+        }).then(postgisResponse => {
+            postgisData = postgisResponse.data;
+
+            // Display results
+            infoDiv.innerHTML = `<h4>Clicked Point: ${latlng.lat}, ${latlng.lng}</h4>`;
+            mosaicData.forEach((data, index) => {
+                infoDiv.innerHTML += `<h4>ImageMosaic Data (Layer ${params.layers[index]}):</h4><pre>${JSON.stringify(data, null, 2)}</pre>`;
+            });
+            infoDiv.innerHTML += `<h4>PostGIS Data:</h4><pre>${JSON.stringify(postgisData, null, 2)}</pre>`;
+        }).catch(error => {
+            console.error('Error fetching data:', error);
+        });
+    });
+
+    // Initialize the map layers
+    updateMap();
 });
---
+++ sil/raster.sld
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:gml="http://www.opengis.net/gml" xmlns:sld="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" version="1.0.0">
+  <UserLayer>
+    <sld:LayerFeatureConstraints>
+      <sld:FeatureTypeConstraint/>
+    </sld:LayerFeatureConstraints>
+    <sld:UserStyle>
+      <sld:Name>IODC_202308150845_HRV_aoi</sld:Name>
+      <sld:FeatureTypeStyle>
+        <sld:Rule>
+          <sld:RasterSymbolizer>
+            <sld:ChannelSelection>
+              <sld:GrayChannel>
+                <sld:SourceChannelName>1</sld:SourceChannelName>
+                <sld:ContrastEnhancement>
+                  <sld:Normalize>
+                    <sld:VendorOption name="algorithm">StretchToMinimumMaximum</sld:VendorOption>
+                    <sld:VendorOption name="minValue">27</sld:VendorOption>
+                    <sld:VendorOption name="maxValue">200</sld:VendorOption>
+                  </sld:Normalize>
+                </sld:ContrastEnhancement>
+              </sld:GrayChannel>
+            </sld:ChannelSelection>
+            <sld:ColorMap>
+              <sld:ColorMapEntry color="#000000" quantity="0"/>
+              <sld:ColorMapEntry color="#ffffff" quantity="255"/>
+            </sld:ColorMap>
+          </sld:RasterSymbolizer>
+        </sld:Rule>
+      </sld:FeatureTypeStyle>
+    </sld:UserStyle>
+  </UserLayer>
+</StyledLayerDescriptor>
