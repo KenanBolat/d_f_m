1e8cea5772d0fe822094d8b3b34b4aa124043456
Kenan Bolat <kenan.bolat@yandex.com.tr>
2024-07-12 16:26:48 +0300

    update animation

--- sil/app.js
+++ sil/app.js
@@ -6,34 +6,35 @@ document.addEventListener("DOMContentLoaded", function () {
         attribution: '&copy; OpenStreetMap contributors'
     }).addTo(map);
 
-
-
-    googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
         maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3']
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
     });
 
-    googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
         maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3']
-});
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
 
-const noBasemapLayer = L.layerGroup();
+    const noBasemapLayer = L.layerGroup();
 
-googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+    googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}', {
         maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3']
-});
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
 
-
-    googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
         maxZoom: 20,
-        subdomains:['mt0','mt1','mt2','mt3']
-});
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
 
     const imageMosaicLayerUrl = 'http://localhost:8080/geoserver/tmet/wms';
     const postgisLayerUrl = 'http://localhost:8080/geoserver/tmet/wms';
     let currentLayers = [];
+    let animationInterval = null;
+    let isPaused = false;
+    let pauseTime = null;
+
     const params = {
         layers: ['tmet:aoi'],  // Add all the layers you need
         format: 'image/png',
@@ -42,7 +43,6 @@ googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
         dim_mission: 'XYZ',
         dim_channel: 'WV_062'
     };
-    
 
     const initializeMapLayers = () => {
         currentLayers.forEach(layer => map.removeLayer(layer));
@@ -56,24 +56,16 @@ googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
                 DIM_CHANNEL: params.dim_channel
             }).addTo(map);
         });
-
     };
-
-    
 
     const infoDiv = document.getElementById('info');
     const legendImage = document.getElementById('legend-image');
 
-
-    
     const updateLegend = () => {
         const legendUrl = `${imageMosaicLayerUrl}?SERVICE=WMS&VERSION=1.1.0&REQUEST=GetLegendGraphic&LAYER=${params.layers[0]}&format=image/png&STYLE=&DIM_MISSION=${params.dim_mission}&DIM_CHANNEL=${params.dim_channel}`;
         document.getElementById('legend-image').src = legendUrl;
-        debugger;
         const legendLabel = document.getElementById('legend-label');
-        legendLabel.innerText = `Mission: ${params.dim_mission}, Channel: ${params.dim_channel}, Date: ${params.time}`;
-
-
+        legendLabel.innerText = `Mission: ${params.dim_mission},  Channel: ${params.dim_channel}, Date: ${params.time}`;
     };
 
     const updateMap = () => {
@@ -81,28 +73,26 @@ googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
         updateLegend();
     };
 
-
     const formatDateToLocalISO = (date) => {
         const tzoffset = date.getTimezoneOffset() * 60000; //offset in milliseconds
         const localISOTime = (new Date(date - tzoffset)).toISOString().slice(0, -1);
-        return localISOTime+'Z';
+        return localISOTime + 'Z';
     };
 
     let startDatePicker = flatpickr("#start-date", {
         enableTime: true,
         dateFormat: "Y-m-d H:i:S",
         defaultDate: "2023-08-14 08:45:00",
-        onChange: function(selectedDates, dateStr, instance) {
+        onChange: function (selectedDates, dateStr, instance) {
             params.time = selectedDates[0] ? formatDateToLocalISO(selectedDates[0]) : '';
-
         }
     });
 
     let endDatePicker = flatpickr("#end-date", {
         enableTime: true,
         dateFormat: "Y-m-d H:i:S",
-        defaultDate: "2023-08-14 08:45:00",
-        onChange: function(selectedDates, dateStr, instance) {
+        defaultDate: "2023-08-15 08:45:00",
+        onChange: function (selectedDates, dateStr, instance) {
             params.end_time = selectedDates[0] ? formatDateToLocalISO(selectedDates[0]) : '';
         }
     });
@@ -110,7 +100,6 @@ googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
     document.getElementById('search-button').addEventListener('click', function () {
         params.dim_mission = document.getElementById('mission-select').value;
         params.dim_channel = document.getElementById('channel-select').value;
-        debugger
         if (params.time === '') {
             const selectedDate = startDatePicker.selectedDates[0];
             params.time = selectedDate ? formatDateToLocalISO(selectedDate) : formatDateToLocalISO(new Date());
@@ -122,8 +111,9 @@ googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
         const start = new Date(startDate);
         const end = new Date(endDate);
         let currentTime = start;
+        isPaused = false;
 
-        const animationInterval = setInterval(() => {
+        const runAnimation = () => {
             if (currentTime > end) {
                 clearInterval(animationInterval);
                 return;
@@ -131,11 +121,36 @@ googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
 
             params.time = formatDateToLocalISO(currentTime);
             initializeMapLayers();
-        
             endDatePicker.setDate(currentTime);
+            updateLegend();
 
             currentTime.setMinutes(currentTime.getMinutes() + 15); // Update time by 15 minutes
-        }, interval);
+        };
+
+        animationInterval = setInterval(runAnimation, interval);
+
+        document.getElementById('pause-button').addEventListener('click', function () {
+            if (animationInterval) {
+                clearInterval(animationInterval);
+                pauseTime = currentTime;
+                isPaused = true;
+            }
+        });
+
+        document.getElementById('play-button').addEventListener('click', function () {
+            if (isPaused) {
+                currentTime = pauseTime;
+                animationInterval = setInterval(runAnimation, interval);
+                isPaused = false;
+            }
+        });
+
+        document.getElementById('stop-button').addEventListener('click', function () {
+            if (animationInterval) {
+                clearInterval(animationInterval);
+                isPaused = false;
+            }
+        });
     };
 
     document.getElementById('start-animation').addEventListener('click', function () {
@@ -148,7 +163,6 @@ googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
             animateMap(startDate, endDate);
         }
     });
-
 
     map.on('click', function (e) {
         const latlng = e.latlng;
@@ -233,15 +247,13 @@ googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
         });
     });
 
-
     const baseMaps = {
-        "Open Street Map": osmLayer, 
+        "Open Street Map": osmLayer,
         "Google Streets": googleStreets,
         "Google Terrain": googleTerrain,
         "Google Satellite": googleSat,
-        "Google Hybrid": googleHybrid, 
+        "Google Hybrid": googleHybrid,
         "Empty": noBasemapLayer
-
     };
 
     const overlayMaps = {
@@ -252,43 +264,42 @@ googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
         })
     };
 
-     // Add layer control to the map
-     L.control.layers(baseMaps, overlayMaps).addTo(map);
+    // Add layer control to the map
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
 
     // Initialize the map layers
     updateMap();
-  // Initialize Leaflet Sidebar
-  const sidebar = L.control.sidebar('sidebar', {
-    closeButton: true,
-    position: 'left'
-}).addTo(map);
 
-// Add AOI layer control to the sidebar
-const layerControlDiv = document.getElementById('layer-control');
-params.layers.forEach(layer => {
-    const layerItem = document.createElement('div');
-    layerItem.innerHTML = `<input type="checkbox" id="${layer}" checked> <label for="${layer}">${layer}</label>`;
-    layerControlDiv.appendChild(layerItem);
+    // Initialize Leaflet Sidebar
+    const sidebar = L.control.sidebar('sidebar', {
+        closeButton: true,
+        position: 'left'
+    }).addTo(map);
 
-    document.getElementById(layer).addEventListener('change', function (e) {
-        if (e.target.checked) {
-            currentLayers.find(l => l.wmsParams.layers === layer).addTo(map);
-        } else {
-            map.removeLayer(currentLayers.find(l => l.wmsParams.layers === layer));
-        }
+    // Add AOI layer control to the sidebar
+    const layerControlDiv = document.getElementById('layer-control');
+    params.layers.forEach(layer => {
+        const layerItem = document.createElement('div');
+        layerItem.innerHTML = `<input type="checkbox" id="${layer}" checked> <label for="${layer}">${layer}</label>`;
+        layerControlDiv.appendChild(layerItem);
+
+        document.getElementById(layer).addEventListener('change', function (e) {
+            if (e.target.checked) {
+                currentLayers.find(l => l.wmsParams.layers === layer).addTo(map);
+            } else {
+                map.removeLayer(currentLayers.find(l => l.wmsParams.layers === layer));
+            }
+        });
     });
-});
 
-// Show the sidebar initially
-sidebar.open('home');
-const legendControl = L.control({ position: 'topright' });
+    // Show the sidebar initially
+    sidebar.open('home');
+    const legendControl = L.control({ position: 'topright' });
     legendControl.onAdd = function (map) {
         const div = L.DomUtil.create('div', 'legend');
-        div.innerHTML = '<h4>Legend</h4><img id="legend-image" src="" alt="Legend">';
+        div.innerHTML = '<h4>Legend</h4><img id="legend-image" src="" alt="Legend"><div id="legend-label"></div>';
         return div;
     };
     legendControl.addTo(map);
     updateLegend();
-
-
 });
