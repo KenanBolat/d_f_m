d337d05d2f7f9c88a5fd80b47894c09424b04eba
Kenan Bolat <kenan.bolat@yandex.com.tr>
2024-07-22 16:09:38 +0300

    check automation

--- scripts/watch_and_update.sh
+++ scripts/watch_and_update.sh
@@ -4,18 +4,21 @@ while getopts 'arch' opt; do
   case "$opt" in
     a)
       echo "Processing option 'a'"
-      DIRECTORY_TO_WATCH="/home/knn/Desktop/d_f_m/j_data/geoserver_data/data/aoi"
-      DIRECTORY_TO_UPDATE="/opt/geoserver_data/data/aoi"
+      DIRECTORY_TO_WATCH="/home/knn/Desktop/d_f_m/j_data/geoserver_data/data/aoi/"
+      DIRECTORY_TO_UPDATE="/opt/geoserver_data/data/aoi/"
+      COVERAGESTORE="tmet"
       ;;
     r)
       echo "Processing option 'rgb'"
-      DIRECTORY_TO_WATCH="/home/knn/Desktop/d_f_m/j_data/geoserver_data/data/rgb"
-      DIRECTORY_TO_UPDATE="/opt/geoserver_data/data/rgb"
+      DIRECTORY_TO_WATCH="/home/knn/Desktop/d_f_m/j_data/geoserver_data/data/rgb/"
+      DIRECTORY_TO_UPDATE="/opt/geoserver_data/data/rgb/"
+      COVERAGESTORE="rgb"
       ;;
     c)
       echo "Processing option 'c' argument"
-      DIRECTORY_TO_WATCH="/home/knn/Desktop/d_f_m/j_data/geoserver_data/data/cloud"
-      DIRECTORY_TO_UPDATE="/opt/geoserver_data/data/cloud"
+      DIRECTORY_TO_WATCH="/home/knn/Desktop/d_f_m/j_data/geoserver_data/data/cloud/"
+      DIRECTORY_TO_UPDATE="/opt/geoserver_data/data/cloud/"
+      COVERAGESTORE="cloud"
       ;;
    
     ?|h)
@@ -29,7 +32,6 @@ shift "$(($OPTIND -1))"
 
 GEOSERVER_URL="http://localhost:8080/geoserver"
 WORKSPACE="tmet"
-COVERAGESTORE="tmet"
 USERNAME="admin"
 PASSWORD="geoserver"
 
