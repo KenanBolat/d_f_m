ed7de24b7aeaeaf193028f6b47e4b972a2f1740e
ardacetinkaya <arda.cetinkaya@valuespaces.com>
2024-09-17 22:59:34 +0200

    add loading after login

--- webmap/src/app/app.routes.ts
+++ webmap/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MapComponent } from './map/map.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { LoadingComponent } from './loading/loading.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent }, // Ensure no leading slash
+  { path: 'loading', component: LoadingComponent, canActivate: [AuthGuard] },
   {
     path: '',
     component: MapComponent,
