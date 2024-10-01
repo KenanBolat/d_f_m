6b2c7332d208c45e986699b0c12b53c92956f8e7
KenanBolat <kenan23@gmail.com>
2024-10-01 23:55:04 +0300

    add dir monitor

---
+++ scripts/monitor_disk_usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set the threshold percentage (e.g., 80%)
+THRESHOLD=80
+OLDER_THAN_DAYS=2
+
+# Get the current usage percentage of the / partition (without the % sign)
+USAGE=$(df / | grep '/' | awk '{print $5}' | sed 's/%//')
+
+# Check if the usage exceeds the threshold
+if [ "$USAGE" -gt "$THRESHOLD" ]; then
+    echo "Disk usage on / has exceeded ${THRESHOLD}%. Running cleanup script..."
+
+    # Run the cleanup script with the specified parameters
+    /media/external/d_f_m/scripts/remove_old_files_from_directory.sh /mnt/data/ tmp  $OLDER_THAN_DAYS
+    /media/external/d_f_m/scripts/remove_old_files_from_directory.sh /mnt/data/ eumetcast  $OLDER_THAN_DAYS
+else
+    echo "Disk usage on / is under control (${USAGE}%). No action needed."
+fi
